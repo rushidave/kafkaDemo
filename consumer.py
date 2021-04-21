df2d99055e423f9d2505b706a3e72c21e44ab47d
Rushi Dave <rushijdave96@gmail.com>
2021-04-21 15:52:05 -0400

    second commit

--- consumer.py
+++ consumer.py
@@ -3,13 +3,18 @@ import json
 import helper
 from psycopg2.extras import RealDictCursor
 import psycopg2
+from dotenv import load_dotenv
+load_dotenv()
+import os
 
 
+uri = os.environ.get("uri")
 def getConsumer():
 
 
-
-    uri = "postgres://avnadmin:y7n6ux2p3ut6hbg7@pg-ff8016c-rushijdave96-69df.aivencloud.com:15480/defaultdb?sslmode=require"
+    
+    print(uri)
+    # uri = "postgres://avnadmin:y7n6ux2p3ut6hbg7@pg-ff8016c-rushijdave96-69df.aivencloud.com:15480/defaultdb?sslmode=require"
 
     db_conn = psycopg2.connect(uri)
     c = db_conn.cursor()
